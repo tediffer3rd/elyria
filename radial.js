cb13168551420141c23a64ec74d992ee4f2c8d84
tediffer3rd <ted.nidiffer@gmail.com>
2022-03-22 11:47:45 -0600

    Add files via upload
    added persistence via Client-side storage - 	localStorage.setItem('saveDate', currentDate);

--- radial.js
+++ radial.js
@@ -4,11 +4,6 @@ let weekdays = ["Nehrdas", "Jahdas", "Gahldas", "Dehrdas", "Elimdas", "Iadas", "
 let seasons = ["Sepren", "Somner", "Autun", "Wevner"];
 let easyDays = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth', 'twnetieth', 'twenty-first', 'twenty-second', 'twenty-third', 'twenty-fourth', 'twenty-fifth', 'twenty-sixth', 'twenty-seventh', 'twenty-eighth', 'twenty-ninth','thitieth', 'thirty-first', 'thirty-second'];
 
-function stringifyNumber(n) {
-  if (n < 20) return special[n];
-  if (n%10 === 0) return deca[Math.floor(n/10)-2] + 'ieth';
-  return deca[Math.floor(n/10)-2] + 'y-' + special[n%10];
-}
 
 let data =  [
 	Array.from(dayNums).map(String),
@@ -22,7 +17,7 @@ let data =  [
 ]
 
 function getRandomHTMLColor() {
-	var r = 255*Math.random()|0;
+	let r = 255*Math.random()|0;
 		g = 255*Math.random()|0;
 		b = 255*Math.random()|0;
 	return 'rgb(' + r + ',' + g + ',' + b + ')';
@@ -85,52 +80,66 @@ function createPieMenu() {
 	document.body.appendChild(pieMenu);
 }
 
- createPieMenu();
- 
-function setDate() {
+createPieMenu();
+
+  // check whether the 'saveDate' data item is stored in web Storage
+  if(localStorage.getItem('saveDate')) {
+    const date = localStorage.getItem('saveDate');
+    document.getElementById('input_id').value=date;
 	var currentDate = document.getElementById('input_id').value;
-	var month = Number(currentDate.slice(0,2))
-	var season = Number(Math.ceil(month/4))
-	var day = Number(currentDate.slice(-2))
-    var weekday = weekdays[day-1]
-	document.getElementById('alert1').innerHTML = 'The current date is: ' + weekday + ' the ' + easyDays[day-1] + ' day of ' + months[month-1] + ' the season of ' + seasons[season-1];
-//    document.getElementById('alert1').innerHTML = 'The current date is: ' + ("0" + month).slice(-2) + " - " + ("0" + day).slice(-2);
+	var month = Number(currentDate.slice(0,2));
+	var season = Number(Math.ceil(month/4));
+	var day = Number(currentDate.slice(-2));
+    var weekday = weekdays[day-1];
+	document.getElementById('alert1').innerHTML = 'Press Enter to set the dial hands';
+//	document.getElementById('alert1').innerHTML = 'The current date is: ' + weekday + ' the ' + easyDays[day-1] + ' day of ' + months[month-1] + ' the season of ' + seasons[season-1];
+//	document.getElementById('alert1').innerHTML = 'The current date is: ' + ("0" + season).slice(-2) + " - " + ("0" + month).slice(-2) + " - " + ("0" + day).slice(-2);
     hour_rotation = (360/4 * season)-((360/4)/2);
 	min_rotation = (360/16 * month)-((360/16)/2);
 	sec_rotation = (360/32 * day)-((360/32)/2);
     hour.style.transform = `rotate(${hour_rotation}deg)`;
 	minute.style.transform = `rotate(${min_rotation}deg)`;
     second.style.transform = `rotate(${sec_rotation}deg)`;
-	
+	localStorage.setItem('saveDate', currentDate);
+  }
+
+function setDate() {
+	var currentDate = document.getElementById('input_id').value;
+	var month = Number(currentDate.slice(0,2));
+	var season = Number(Math.ceil(month/4));
+	var day = Number(currentDate.slice(-2));
+    var weekday = weekdays[day-1];
+	document.getElementById('alert1').innerHTML = 'The current date is: ' + weekday + ' the ' + easyDays[day-1] + ' day of ' + months[month-1] + ' the season of ' + seasons[season-1];
+//    document.getElementById('alert1').innerHTML = 'The current date is: ' + ("0" + season).slice(-2) + " - " + ("0" + month).slice(-2) + " - " + ("0" + day).slice(-2);
+    hour_rotation = (360/4 * season)-((360/4)/2);
+	min_rotation = (360/16 * month)-((360/16)/2);
+	sec_rotation = (360/32 * day)-((360/32)/2);
+    hour.style.transform = `rotate(${hour_rotation}deg)`;
+	minute.style.transform = `rotate(${min_rotation}deg)`;
+    second.style.transform = `rotate(${sec_rotation}deg)`;
+	localStorage.setItem('saveDate', currentDate);
 }
 
 function addDay() {
 	var currentDate = document.getElementById('input_id').value;
-	var month = Number(currentDate.slice(0,2))
-	var day = Number(currentDate.slice(-2))
-	var nextDay = day + 1;
-		if(nextDay == 33) {month += 1, nextDay = 1 ;}
-		if(month == 17) {month = 1 , nextDay = 1 ;}
-	var season = Number(Math.ceil(month/4))
+	var month = Number(currentDate.slice(0,2));
+	var day = Number(currentDate.slice(-2));
+	var day = day + 1;
+		if(day == 33) {month += 1, day = 1};
+		if(month == 17) {month = 1 , day = 1};
+	var season = Number(Math.ceil(month/4));
     hour_rotation = (360/4 * season)-((360/4)/2);
     min_rotation = (360/16 * month)-((360/16)/2);
-	sec_rotation = (360/32 * nextDay)-((360/32)/2);
+	sec_rotation = (360/32 * day)-((360/32)/2);
 	hour.style.transform = `rotate(${hour_rotation}deg)`;
     minute.style.transform = `rotate(${min_rotation}deg)`;
     second.style.transform = `rotate(${sec_rotation}deg)`;
-	var day = nextDay
-//	var monthName = 
-	document.getElementById('input_id').value=("0" + month).slice(-2) + ("0" + day).slice(-2);
-//    document.getElementById('alert1').innerHTML = 'The current date is: ' + ("0" + month).slice(-2) + " - " + ("0" + day).slice(-2);
-    var weekday = weekdays[day-1]
+	var newDate = ("0" + month).slice(-2) + ("0" + day).slice(-2);
+	document.getElementById('input_id').value = newDate;
+    var weekday = weekdays[day-1];
 	document.getElementById('alert1').innerHTML = 'The current date is: ' + weekday + ' the ' + easyDays[day-1] + ' day of ' + months[month-1] + ' the season of ' + seasons[season-1];
+//    document.getElementById('alert1').innerHTML = 'The current date is: ' + ("0" + season).slice(-2) + " - " + ("0" + month).slice(-2) + " - " + ("0" + day).slice(-2);
+	localStorage.setItem('saveDate', newDate);
 
 }	
 
-const input = document.querySelector('input_id')
-input.onkeydown = (e) => {
-  if (e.keyCode === 13) {
-    e.preventDefault()
-    console.log('submit')
-  }
-};
