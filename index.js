88ca1a50cdceda95a7d68de0ead03d4041b06777
tediffer3rd <ted.nidiffer@gmail.com>
2023-11-18 18:19:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 //set arrays
-var seasons = ["Sepren" + "<br" +"(equinox)", "Somner (solstice)", "Autun (equinox)", "Wevner (sostice)"];
+var seasons = ["Sepren" + "<br>" + "(equinox)", "Somner" + <br> + "(solstice)", "Autun (equinox)", "Wevner (sostice)"];
 var sunPhase = ["equinox", "solstice", "equinox", "solstice"];
 var months = ["Solaris", "Seprensdor", "Fonsoc", "Ganrehm", "Calidum", "Somnercrest", "Aesoc", "Jehmri", "Lunaris", "Autunsveil", "Cadoc", "Nehnma", "Frigus", "Wevnercrest", "Hemoc", "Duhmret"];
 var time = ["Noon", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm", "8 pm", "9 pm", "10 pm", "11 pm", "Midnight", "1 am", "2 am", "3 am", "4 am", "5 am", "6 am", "7 am", "8 am", "9 am", "10 am", "11 am" ];
