571db1c23ff67365ed8ca2ec71ad3ca861bded86
tediffer3rd <ted.nidiffer@gmail.com>
2023-11-18 17:58:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 //set arrays
 var seasons = ["Sepren", "Somner", "Autun", "Wevner"];
+var sunPhase = ["equinox", "solstice", "equinox", "solstice"];
 var months = ["Solaris", "Seprensdor", "Fonsoc", "Ganrehm", "Calidum", "Somnercrest", "Aesoc", "Jehmri", "Lunaris", "Autunsveil", "Cadoc", "Nehnma", "Frigus", "Wevnercrest", "Hemoc", "Duhmret"];
 var time = ["Noon", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm", "8 pm", "9 pm", "10 pm", "11 pm", "Midnight", "1 am", "2 am", "3 am", "4 am", "5 am", "6 am", "7 am", "8 am", "9 am", "10 am", "11 am" ];
 var dayNums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32"];
@@ -13,6 +14,7 @@ var data =  [
 	Array.from(dayNums).map(String),
 	Array.from(months).map(String),
 	Array.from(seasons).map(String),
+	Array.from(sunPhase).map(String),
 	]
 
 //set variables for calendar
@@ -321,11 +323,6 @@ var moonMaskPath = document.createElementNS("http://www.w3.org/2000/svg", "path"
 	if (day <=16) {var sweep1 = " 0 0 0," ; var sweep2 = " 0 0 1,"} else
 	if (day <=24) {var sweep1 = " 0 0 1," ; var sweep2 = " 0 0 0,"} else
 	if (day <=32) {var sweep1 = " 0 0 1," ; var sweep2 = " 0 0 1,"};
-
-//	if (day <=08) {var sweep1 = " 0 0 1," ; var sweep2 = " 0 0 1,"} else
-//	if (day <=16) {var sweep1 = " 0 0 1," ; var sweep2 = " 0 0 0,"} else
-//	if (day <=24) {var sweep1 = " 0 0 0," ; var sweep2 = " 0 0 1,"} else
-//	if (day <=32) {var sweep1 = " 0 0 0," ; var sweep2 = " 0 0 0,"};
 
 // set the arc of moonMask
 	if (day == 01 || day == 16 || day == 17 || day == 32) {var rad = " 100 100,"} else
